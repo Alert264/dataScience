c1aa4ae826652829cc81b8eec624fb585c4b7065
涛 <2641878548@qq.com>
2020-12-30 20:54:51 +0800

    Update split_words.py
    修改储存格式，word_list添加title

--- split_words.py
+++ split_words.py
@@ -15,21 +15,23 @@ def int_to_date(integer):
 def save_word_list(start_date, days, root_path):
     for i in range(days):
         date = int_to_date(date_to_int(start_date) + 86400 * i)
+        print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + "]:" + "正在分词：" + date)
         corpus = []
-        filename = root_path + '/' + date + ".json"
+        filename = 'source/' + root_path + '/' + date + ".json"
         with open(filename, 'r', encoding='utf-8') as File:
             data = json.load(File).get("news")
         for part in data:
             new = ""
+            title = part.get('title')
             word_list = get_paticle_words(part.get('text'))
             if (judge_relativeness(word_list)):
                 new = " ".join(word_list)
-                corpus.append(new)
+                corpus.append({"title": title, "word_list": new})
         filename = 'word_list' + '/' + root_path + '/' + date + ".json"
         with open(filename, 'w+', encoding='utf-8') as f:
             json.dump(corpus, f, ensure_ascii=False, indent=4)
-        print(date + ": 写入分词完成。")
-    print("从" + str(start_date) + "开始，共" + str(days) + "天的分词完成")
+        print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + "]:" + date + ": 写入分词完成。")
+    print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + "]:" + "从" + str(start_date) + "开始，共" + str(days) + "天的分词完成")
 
 
 if __name__ == '__main__':
