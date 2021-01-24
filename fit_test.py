81bdb19e77988ca5b1f4a642f39e96a8274fa80c
涛 <2641878548@qq.com>
2021-01-24 16:57:41 +0800

    Update fit_test.py

--- fit_test.py
+++ fit_test.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plot
 import numpy as np
 import json
 import os
-def divide_data(filesource='source/rmrb',gap=50,length=10000):
+import pandas
+def divide_data(filesource='source/rmrb',gap=20,length=10000):
     filenames = os.listdir(filesource)
     limit = (int)(length/gap)
     res = []
     left = 0
     count = 0
     front = 0
-    frontLimit = 5
+    frontLimit = 1
     for element in range(int(length/gap)+1):
         res.append(0)
     for filepath in filenames:
@@ -29,20 +30,28 @@ def divide_data(filesource='source/rmrb',gap=50,length=10000):
             print(filepath)
 
 
+    frontNum = 0
+    for element in range(0,frontLimit):
+        frontNum += res[element]
     x=[]
+    print(frontNum)
     for element in range(0,limit+1):
         x.append(element * gap)
     print(front)
+    dic1 = {'num_gap':x,'num_of_each':res}
+    df = pandas.DataFrame(dic1)
+    df.to_excel('rmrb.xlsx',index=False)
+
 
     plot.plot(x,res)
     plot.show()
     print('more than {}: {}'.format(length,left))
 
     print("total:{}".format(count))
-    print("front {} make up for {:.4f}%".format(frontLimit*gap,100*gap*frontLimit/count))
+    print("front {} make up for {:.4f}%".format(frontLimit*gap,frontNum/count*100))
 
 
 
 
 if __name__ == '__main__':
-    divide_data('source/ysxw',110)
+    divide_data('source/xlxw',100)
